085d7bc032a2a0f49c802ae70bcd2d0369a904c8
Anirudh Anand <69196828+anirudhanand04@users.noreply.github.com>
2022-06-05 21:23:15 +0530

    Create Songs.cpp

---
+++ codechef/Songs.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;cin>>t;
+	while(t--){
+	    int n,x;
+	    cin>>n>>x;
+	    x=x*3;
+	    int sol=n/x;
+	    cout<<sol<<endl;
+	}
+	return 0;
+}
